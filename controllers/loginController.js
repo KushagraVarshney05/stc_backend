392ed72c66bd89cfa603dad915535ca22553a5a5
Akhil Raj Srivastava <84132589+GeekCoderRaj@users.noreply.github.com>
2023-04-28 03:22:54 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -12,13 +12,13 @@ const loginController = {
             try{
                 const data = await db.promise().query(`Select * from StudentTable where email = '${LibraryID}'`);
                 if(!data[0]){
-                   // console.log("HI")
+                   console.log("HI")
                     return next(CustomErrorHandler.wrongCredentials());
                 }
                 
                 if(Password !== data[0][0].Password)
                 {
-                    //console.log(Password, data[0].Password)
+                    console.log(Password, data[0].Password)
                     return next(CustomErrorHandler.wrongCredentials());
                 }
                 const access_token = JwtService.sign({LibraryID,data: data[0][0].userName });
