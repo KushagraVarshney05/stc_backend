9991ae802103e9e6562e848ae10fad087ee7aa52
Yas-Var <yashvarshney7011@gmail.com>
2024-01-03 10:49:10 +0530

    completed the company page

--- controllers/Admin/company.js
+++ controllers/Admin/company.js
@@ -23,11 +23,9 @@ const addCompany = async (req, res) => {
   }
 };
 
-const getCompanyData = async (req,res,next) => {
+const getCompanyData = async (req, res, next) => {
   try {
-      const data = await db
-      .promise()
-      .query(`SELECT *
+    const data = await db.promise().query(`SELECT *
       FROM company
       LEFT JOIN class ON company.companyClass = class.classID
       LEFT JOIN users ON company.companyAddedBy = users.userID`);
@@ -35,78 +33,90 @@ const getCompanyData = async (req,res,next) => {
     if (!data[0]) {
       return next(CustomErrorHandler.wrongCredentials());
     }
-    
 
     return res.status(200).json({ success: true, data });
   } catch (error) {
-      return next(CustomErrorHandler.serverError());
-      }
+    return next(CustomErrorHandler.serverError());
+  }
 };
-const updateCompany = async (req,res,next) => {
-  const {id} = req.params;
+const updateCompany = async (req, res, next) => {
+  const { id } = req.params;
   console.log(id);
+  console.log(req.body);
+
   const {
     companyName,
-    companyClass,
     companyDescription,
     companyEstablishment,
     companyWebsite,
-    companyAddedBy,
-    imageID,
   } = req.body;
 
   try {
     const data = await db
       .promise()
       .query(
-        `UPDATE company SET companyName = '${companyName}', companyClass = '${companyClass}', companyDescription = '${companyDescription}', companyEstablishment = '${companyEstablishment}', companyWebsite = '${companyWebsite}', companyAddedBy = '${companyAddedBy}' WHERE companyID = ${parseInt(id)}`
+        `UPDATE company SET companyName = ?, companyDescription = ?, companyEstablishment = ?, companyWebsite = ? WHERE companyID = ?`,
+        [
+          companyName,
+          companyDescription,
+          companyEstablishment,
+          companyWebsite,
+          id,
+        ]
       );
 
     res.status(201).json({ data: data[0] });
   } catch (e) {
+    console.log(e);
     res.status(400).json({ error: e });
   }
 };
 const deleteCompany = async (req, res, next) => {
-  const Id = req.body.CompanyID;
-  console.log(req.body.CompanyID);
+  const { id } = req.params;
+  console.log(id);
+
   try {
-    const data = await db
+    // Delete child rows first
+    await db
       .promise()
       .query(
-        `DELETE FROM company WHERE companyID IN (${Id.map((id) => parseInt(id)).join(",")})`
+        `DELETE FROM companydatadivided WHERE companyID = ${parseInt(id)}`
       );
+
+    // Delete the company record
+    const data = await db
+      .promise()
+      .query(`DELETE FROM company WHERE companyID = ${parseInt(id)}`);
 
     res.status(201).json({ data: data[0] });
   } catch (e) {
+    console.log(e);
     res.status(400).json({ error: e });
   }
 };
-const getCompany = async (req,res,next) => {
-
-  const {id} = req.params;
+const getCompany = async (req, res, next) => {
+  const { id } = req.params;
   console.log(id);
   try {
-    const data = await db
-      .promise()
-      .query(
-        `SELECT *
+    const data = await db.promise().query(
+      `SELECT *
         FROM company
         LEFT JOIN class ON company.companyClass = class.classID
-        LEFT JOIN users ON company.companyAddedBy = users.userID WHERE company.companyID = ${parseInt(id)}`
-      );
+        LEFT JOIN users ON company.companyAddedBy = users.userID WHERE company.companyID = ${parseInt(
+          id
+        )}`
+    );
 
     res.status(201).json({ data: data[0] });
   } catch (e) {
     res.status(400).json({ error: e });
   }
 };
-
 
 module.exports = {
   addCompany,
   getCompanyData,
   updateCompany,
   deleteCompany,
-  getCompany
+  getCompany,
 };
--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 const adminAuth = require("../middlewares/admin_auth");
 
-const { addCompany,
-    getCompanyData,
-    updateCompany,
-    deleteCompany,
-    getCompany  } = require("../controllers/Admin/company");
+const {
+  addCompany,
+  getCompanyData,
+  updateCompany,
+  deleteCompany,
+  getCompany,
+} = require("../controllers/Admin/company");
 
 router.post("/loginAdmin", loginAdmin);
 router.post("/registerAdmin", adminAuth, registerAdmin);
@@ -17,7 +19,6 @@ router.get("/getCompanyData", adminAuth, getCompanyData);
 router.post("/addCompany", adminAuth, addCompany);
 router.post("/updateCompany/:id", adminAuth, updateCompany);
 router.get("/getCompany/:id", adminAuth, getCompany);
-router.delete("/deleteCompany", adminAuth, deleteCompany);
-
+router.get("/deleteCompany/:id", adminAuth, deleteCompany);
 
 module.exports = router;
